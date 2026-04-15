69ab50f52013477dfb6e4615fee9902f503d61c1
msidhu-deep <mehakdeepsidhu2006@gmail.com>
2026-04-15 16:24:57 -0600

    Create Bookings done

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,1 +1,24 @@
-const Booking = require("../models/Booking");
+const Booking = require("../config/db.js");
+
+// ===========Create Bookings =============
+export const createBooking = async (req, res) => {
+  try {
+    const { propertyId, date, time, status } = req.body;
+    const userId = req.user.id;
+    const [result] = await pool.query(
+      `INSERT INTO bookings
+      (propertyId, userId, date, time, status)
+      VALUES (?, ?, ?, ?,?)`,
+       [propertyId, userId, date, time, status || "pending"]
+        );
+
+        res.json({
+            success: true,
+            bookingId: result.insertId
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ success: false });
+    }
+};
