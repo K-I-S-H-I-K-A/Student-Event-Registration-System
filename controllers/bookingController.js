4be15564a14b0a48445bc912d4c337f21ad48b18
msidhu-deep <mehakdeepsidhu2006@gmail.com>
2026-04-15 16:55:37 -0600

    Add getBookingById function to bookingController
    Added a new function to retrieve a booking by its ID.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -22,3 +22,20 @@ export const createBooking = async (req, res) => {
         res.status(500).json({ success: false });
     }
 };
+
+//Get Booking by Id
+export const getBookingById = async (req, res) =>
+  try {
+  const {id} = req.params;
+  const [rows] = await pool.query(
+     "SELECT * FROM bookings WHERE id = ?",
+    [id]
+    );
+
+if (rows.length === 0) {
+  return res.status(404).json({ message: "Not found"});
+}
+  res.json(rows[0]);
+
+} catch (err) {
+  res.status(500).json({ success: false });
